617e7da0dfdadf92b9e648ecdd9cd71da8c8c2c8
Grantjos12 <grantjoslyn@gmail.com>
2021-03-03 21:06:56 -0600

    API call to mealsDB working

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 const meals = document.getElementById("meals");
+
+window.onload = renderMeals();
+
+function renderMeals() {
+  fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.categories);
+            let render = "";
+            data.categories.map(
+              (movie) =>
+                (render += `
+                <div class="uk-card uk-card-default uk-card-body uk-grid-margin">
+      
+        </div>
+      `
+            );
+            meals.innerHTML = render;
+          });
+    });
+}
