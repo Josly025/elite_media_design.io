c1f2abdd3d4f74a53ed9f7cd5838e0511c7d7c05
Grantjos12 <grantjoslyn@gmail.com>
2021-03-03 21:18:45 -0600

    inital grid and render working

--- js/app.js
+++ js/app.js
@@ -9,16 +9,38 @@ function renderMeals() {
     })
     .then((data) => {
       console.log(data.categories);
-            let render = "";
-            data.categories.map(
-              (movie) =>
-                (render += `
-                <div class="uk-card uk-card-default uk-card-body uk-grid-margin">
-      
-        </div>
-      `
-            );
-            meals.innerHTML = render;
-          });
+      let render = "";
+      data.categories.map(
+        (meal) =>
+          (render += `
+        
+<div  class="uk-card uk-card-default uk-grid-margin uk-card-body uk-grid-margin ">
+    
+ <div class="uk-card-media-top">
+        <img
+          class="dash__image uk-margin-medium-top"
+          src="${meal.strCategoryThumb}"
+          alt=""
+        />
+      </div>
+      <div class="uk-card-body">
+        <h3 class="uk-card-title dash__title--product">${meal.strCategory}</h3>
+      </div>
+        <button
+          class="uk-button uk-button-default uk-margin-medium-bottom dash__cartBtn"
+        >
+          Full Reciepe
+        </button>
+    
+
+</div>
+
+
+
+     
+            
+      `)
+      );
+      meals.innerHTML = render;
     });
 }
